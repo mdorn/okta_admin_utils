0a084d362582d84d8589c94e3978b07592003bab
Matt Dorn <matt.dorn@gmail.com>
2021-10-07 16:58:43 -0500

    Add additional resources for triggering rate limits

--- okta_admin_utils/tasks/trigger_rate_limit_violation.py
+++ okta_admin_utils/tasks/trigger_rate_limit_violation.py
@@ -5,26 +5,33 @@ import click
 from okta_admin_utils.logs import format_json_output
 from okta_admin_utils.api import OktaAPIClient
 
+REQUESTS = 1000
+THREADS = 100
 
-def job(num, api):
-    api.groups.list(params={'limit': 2})
+
+def job(num, api_resource):
+    api_resource.list(params={'limit': 2})
 
 
 class Client(OktaAPIClient):
 
     def run(self):
-        REQUESTS = 1000
-        THREADS = 100
+        resource = click.prompt(
+            'Which endpoint would you like to flood with GET requests?',
+            type=click.Choice(['users', 'groups', 'logs', 'apps']),
+            default='users'
+        )
         confirm = click.prompt(
-            'This will flood the /api/groups endpoint with ~1,000 requests. Are you sure (y/n)?',
+            'This will flood the /api/{} endpoint with ~1,000 requests. Are you sure (y/n)?'.format(resource),
             type=bool,
             default='n'
         )
         if confirm:
-            self.api.add_resource(resource_name='groups')
+            self.api.add_resource(resource_name=resource)
+            api_resource = getattr(self.api, resource)
             args = range(REQUESTS)
             with ThreadPoolExecutor(max_workers=THREADS) as executor:
-                futures = {executor.submit(job, arg, self.api) for arg in args}
+                futures = {executor.submit(job, arg, api_resource) for arg in args}
                 wait(futures)
                 print(len(futures))
                 logging.info('Complete')
